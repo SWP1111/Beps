758f52806f9393935ca018f9723191c07d82f0f3
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2025-04-30 13:09:14 +0900

    파일 api 추가

--- BepsApi/Backend/API/blueprints/__init__.py
+++ BepsApi/Backend/API/blueprints/__init__.py
@@ -7,9 +7,11 @@ def register_blueprints(app):
     from blueprints.contents_routes import api_contents_bp
     from blueprints.memo_routes import api_memo_bp
     from blueprints.memo_reply_routes import api_memo_reply_bp
+    from blueprints.file_routes import file_bp
     
     app.register_blueprint(api_user_bp, url_prefix='/user')
     app.register_blueprint(api_leaning_bp, url_prefix='/leaning')
     app.register_blueprint(api_contents_bp, url_prefix='/contents')
     app.register_blueprint(api_memo_bp, url_prefix='/memo')
     app.register_blueprint(api_memo_reply_bp, url_prefix='/memo/reply')
+    app.register_blueprint(file_bp, url_prefix='/file')
---
+++ BepsApi/Backend/API/file_routes.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, request, jsonify
+from API.models import Files
+from extensions import db
+
+file_bp = Blueprint('file_bp', __name__)
+
+@file_bp.route('/get_file_info', methods=['GET'])
+def get_file_info_by_path():
+    """Get file_id and folder_id for a given file_path"""
+    file_path = request.args.get('file_path')
+    
+    if not file_path:
+        return jsonify({'error': 'Missing file_path parameter'}), 400
+    
+    file = Files.query.filter_by(file_path=file_path, is_deleted=False).first()
+    
+    if not file:
+        return jsonify({'error': 'File not found'}), 404
+    
+    return jsonify({
+        'file_id': file.file_id,
+        'folder_id': file.folder_id
+    })
+
+@file_bp.route('/get_file_path', methods=['GET'])
+def get_file_path_by_ids():
+    """Get file_path for given file_id and folder_id"""
+    file_id = request.args.get('file_id')
+    folder_id = request.args.get('folder_id')
+    
+    if not file_id:
+        return jsonify({'error': 'Missing file_id parameter'}), 400
+    
+    query = Files.query.filter_by(is_deleted=False)
+    
+    if file_id:
+        query = query.filter_by(file_id=file_id)
+    
+    if folder_id:
+        query = query.filter_by(folder_id=folder_id)
+    
+    file = query.first()
+    
+    if not file:
+        return jsonify({'error': 'File not found'}), 404
+    
+    return jsonify({
+        'file_path': file.file_path
+    }) 
